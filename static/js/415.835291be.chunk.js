bcbeb30600ff725280bd79c284ae5a6d24113641
SvitlanaKr22 <109691614+SvitlanaKr22@users.noreply.github.com>
2023-04-26 14:23:57 +0000

    Deploying to gh-pages from @ SvitlanaKr22/project-react-twitter-cards@0031b57765fdc2477d402ba7e5071e2ef492cd5e 🚀

--- static/js/415.835291be.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproject_react_twitter_cards=self.webpackChunkproject_react_twitter_cards||[]).push([[415],{5415:function(t,r,e){e.r(r),e.d(r,{default:function(){return n}});var c=e(184);function n(){return(0,c.jsx)("div",{children:(0,c.jsx)("p",{children:"Twitter cards"})})}}}]);
-//# sourceMappingURL=415.835291be.chunk.js.map
